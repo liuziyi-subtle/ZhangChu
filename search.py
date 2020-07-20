6f9fde5004a1013b00861c5bf0445337bef0a33b
Ziyi Liu <lc_lc2000@vip.163.com>
2020-07-20 19:28:17 +0800

    soybean-and-cotton: 初步实现基于DARTS的AutoML.

--- search.py
+++ search.py
@@ -9,13 +9,14 @@ import torch
 import torch.nn as nn
 
 import datasets
-# from model import CNN
-# from nni.nas.pytorch.callbacks import ArchitectureCheckpoint, LRSchedulerCallback
-# from nni.nas.pytorch.darts import DartsTrainer
-# from utils import accuracy
+from model import CNN
+from nni.nas.pytorch.callbacks import ArchitectureCheckpoint, LRSchedulerCallback
+from nni.nas.pytorch.darts import DartsTrainer
+from utils import accuracy
 
 logger = logging.getLogger('nni')
 
+# python3 -m debugpy --listen 0.0.0.0:5678 --wait-for-client ./search.py --batch-size 8 --epochs 5
 if __name__ == "__main__":
     parser = ArgumentParser("darts")
     parser.add_argument("--layers", default=8, type=int)
@@ -27,31 +28,37 @@ if __name__ == "__main__":
     parser.add_argument("--visualization", default=False, action="store_true")
     args = parser.parse_args()
 
-    # 加载为数据集对象
     dataset_train, dataset_valid = datasets.get_dataset(
-        "/ufs/data/with-zhangchu/results/df_records_cotton.csv")
-    print(dataset_train)
+        "/Users/liuziyi/Documents/ZJU/张初/results/df_records_cotton.csv")
+    print()
+    # dataset_train.data = dataset_train.data[:128, :]
+    # dataset_valid.data = dataset_valid.data[:128, :]
 
-    # model = CNN(32, 3, args.channels, 10, args.layers)
-    # criterion = nn.CrossEntropyLoss()
+    # # model = CNN(32, 3, args.channels, 10, args.layers)
+    # 输入为200*1的1维向量，stem输出通道args.channels=16，8个cell
+    model = CNN(200, 1, args.channels, 7, args.layers)
 
-    # optim = torch.optim.SGD(model.parameters(), 0.025,
-    #                         momentum=0.9, weight_decay=3.0E-4)
-    # lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
-    #     optim, args.epochs, eta_min=0.001)
+    # 损失函数
+    criterion = nn.CrossEntropyLoss()
 
-    # trainer = DartsTrainer(model,
-    #                        loss=criterion,
-    #                        metrics=lambda output, target: accuracy(
-    #                            output, target, topk=(1,)),
-    #                        optimizer=optim,
-    #                        num_epochs=args.epochs,
-    #                        dataset_train=dataset_train,
-    #                        dataset_valid=dataset_valid,
-    #                        batch_size=args.batch_size,
-    #                        log_frequency=args.log_frequency,
-    #                        unrolled=args.unrolled,
-    #                        callbacks=[LRSchedulerCallback(lr_scheduler), ArchitectureCheckpoint("./checkpoints")])
-    # if args.visualization:
-    #     trainer.enable_visualization()
-    # trainer.train()
+    # 优化器
+    optim = torch.optim.SGD(model.parameters(), 0.025,
+                            momentum=0.9, weight_decay=3.0E-4)
+    lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
+        optim, args.epochs, eta_min=0.001)
+
+    trainer = DartsTrainer(model,
+                           loss=criterion,
+                           metrics=lambda output, target: accuracy(
+                               output, target, topk=(1,)),
+                           optimizer=optim,
+                           num_epochs=args.epochs,
+                           dataset_train=dataset_train,
+                           dataset_valid=dataset_valid,
+                           batch_size=args.batch_size,
+                           log_frequency=args.log_frequency,
+                           unrolled=args.unrolled,
+                           callbacks=[LRSchedulerCallback(lr_scheduler), ArchitectureCheckpoint("./checkpoints")])
+    if args.visualization:
+        trainer.enable_visualization()
+    trainer.train()
--- seed.py
+++ seed.py
@@ -1,11 +1,12 @@
 import os
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 
 class SEED():
-    train_list = list(range(500))
-    test_list = list(range(500, 600))
+    # train_list = list(range(500))
+    # test_list = list(range(500, 600))
 
     def __init__(self, path, data_idx=None, train=True, transform=None, target_transform=None):
         self.train = train
@@ -14,12 +15,22 @@ class SEED():
         # TODO: 根据dataset的格式确定不同的组织方式, 最终汇聚成torch公用的格式.
         # 目前, 是按照df_objects的格式读取.
         self.data = raw[[c for c in raw.columns if 'band' in c]].values
-        if data_idx is None:
-            data_idx = self.train_list if self.train else self.test_list
-        self.data = self.data[data_idx, :]
-        self.data = self.data.reshape(-1, 1, self.data.shape[1], 1)
-        self.data = self.data.transpose((0, 1, 2, 3))  # convert to HWC
         self.targets = raw['subtype'].values
+        self.classes = np.unique(self.targets)
+        self.class_to_idx = {_class: i for i,
+                             _class in enumerate(self.classes)}
+
+        self.targets = [self.class_to_idx[x] for x in self.targets]
+
+        if data_idx is None:
+            # data_idx = self.train_list if self.train else self.test_list
+            train_idx, test_idx = train_test_split(
+                self.targets, test_size=0.2, random_state=0)
+            data_idx = train_idx if self.train else test_idx
+
+        self.data = self.data[data_idx, :].astype(np.float32)
+        self.data = self.data.reshape(-1, 1, 200)
+        self.data = self.data.transpose((0, 1, 2))  # convert to HWC
 
         self.transform = transform
         self.target_transform = target_transform
